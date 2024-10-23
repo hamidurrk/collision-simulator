2f11929ab8a1fb09732e8b8c2cbfaeb218065350
hamidurrk <hamidurrk@gmail.com>
2024-10-24 01:50:30 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Image from "next/image";
+import PhysicsPage from "../pages/physics"
+export default function Home() {
+  return (
+    <>
+    <PhysicsPage />
+    </>
+  );
+}
